29c54605d9dfd49703e3ffd3e6e9e7afc214c87c
William Hicks <whicks@nvidia.com>
2023-06-23 18:51:55 -0400

    Integrate device resource changes

--- src/common/raft/raft_utils.cuh
+++ src/common/raft/raft_utils.cuh
@@ -15,14 +15,23 @@
 
 namespace raft_utils {
 
+auto
+get_current_device() {
+    auto result = int{};
+    RAFT_CUDA_TRY(cudaGetDevice(&result));
+    return result;
+}
+
+inline auto thread_counter = std::atomic<std::size_t>{};
+inline auto
+get_thread_id() {
+    thread_local std::size_t id = ++thread_counter;
+    return id;
+}
+
 // TODO(wphicks): Replace this with version from RAFT once merged
 struct device_setter {
-    device_setter(int new_device)
-        : prev_device_{[]() {
-              auto result = int{};
-              RAFT_CUDA_TRY(cudaGetDevice(&result));
-              return result;
-          }()} {
+    device_setter(int new_device) : prev_device_{[]() { return get_current_device(); }()} {
         RAFT_CUDA_TRY(cudaSetDevice(new_device));
     }
 
@@ -34,23 +43,21 @@ struct device_setter {
     int prev_device_;
 };
 
-namespace detail {
-
 inline auto raft_mutex = std::mutex{};
 
 struct gpu_resources {
     gpu_resources(std::size_t streams_per_device = std::size_t{1})
-        : streams_per_device_{streams_per_device}, main_streams_{}, stream_pools_{}, memory_resources_{} {
+        : streams_per_device_{streams_per_device}, stream_pools_{}, memory_resources_{} {
     }
 
     void
-    init(int device_id, ) {
+    init(int device_id = get_current_device()) {
         auto lock = std::lock_guard{raft_mutex};
         auto stream_iter = stream_pools_.find(device_id);
 
         if (stream_iter == stream_pools_.end()) {
             auto scoped_device = device_setter{device_id};
-            stream_pools_[device_id] = rmm::cuda_stream_pool{streams_per_device};
+            stream_pools_[device_id] = std::make_shared<rmm::cuda_stream_pool>(streams_per_device_);
 
             // Set up device memory pool for this device
             auto init_pool_size = thrust::optional<std::size_t>{};
@@ -63,24 +70,16 @@ struct gpu_resources {
                 if (stat == 2) {
                     LOG_KNOWHERE_INFO_ << "Get Gpu Pool Size From env, init size: " << init_pool_size_tmp
                                        << " MB, max size: " << max_pool_size_tmp << " MB";
-                    init_pool_size = initial_pool_size_tmp << 20;
-                    max_pool_size = maximum_pool_size_tmp << 20;
+                    init_pool_size = init_pool_size_tmp << 20;
+                    max_pool_size = max_pool_size_tmp << 20;
                 } else {
                     LOG_KNOWHERE_WARNING_ << "please check env format";
                 }
             }
             memory_resources_[device_id] =
-                std::make_unique<rmm::mr::pool_memory_resource<rmm::mr::cuda_memory_resource>>(
+                std::make_unique<rmm::mr::pool_memory_resource<rmm::mr::device_memory_resource>>(
                     rmm::mr::get_current_device_resource(), init_pool_size, max_pool_size);
             rmm::mr::set_current_device_resource(memory_resources_[device_id].get());
-
-            // Set up raft device_resources for each stream
-            raft_resources_[device_id] = std::vector<raft::device_resources>{};
-            raft_resources_[device_id].reserve(streams_per_thread_);
-            for (auto i = std::size_t{}; i < streams_per_thread_; ++i) {
-                raft_resources_.emplace_back(stream_pools_[device_id].get_stream(i), {nullptr},
-                                             rmm::mr::get_current_device_resource());
-            }
         }
     }
 
@@ -90,46 +89,38 @@ struct gpu_resources {
     }
 
     auto
-    get_stream_view(int device_id, std::size_t thread_id) {
-        return stream_pools_[device_id].get_stream(thread_id % streams_per_device_);
+    get_stream_view(int device_id = get_current_device(), std::size_t thread_id = get_thread_id()) {
+        return stream_pools_[device_id]->get_stream(thread_id % streams_per_device_);
     }
 
  private:
     std::size_t streams_per_device_;
     std::map<int, std::shared_ptr<rmm::cuda_stream_pool>> stream_pools_;
-    std::map<int, std::unique_ptr<rmm::mr::pool_memory_resource<rmm::mr::cuda_memory_resource>>> memory_resources_;
+    std::map<int, std::unique_ptr<rmm::mr::pool_memory_resource<rmm::mr::device_memory_resource>>> memory_resources_;
 };
 
 inline auto&
-get_gpu_resources() {
-    static auto resources = gpu_resources{};
+get_gpu_resources(std::size_t streams_per_device = std::size_t{1}) {
+    static auto resources = gpu_resources{streams_per_device};
     return resources;
 }
 
-inline auto thread_counter = std::atomic<std::size_t>{};
-inline auto
-get_thread_id() {
-    thread_local std::size_t id = ++thread_counter;
-    return id;
+inline void
+init_gpu_resources(std::size_t streams_per_device = std::size_t{1}, int device_id = get_current_device()) {
+    get_gpu_resources(streams_per_device).init(device_id);
 }
 
-}  // namespace detail
-
 inline auto&
-get_raft_resources(int device_id) {
-    thread_local auto all_resources = std::map<int, raft::device_resources>;
+get_raft_resources(int device_id = get_current_device()) {
+    thread_local auto all_resources = std::map<int, std::unique_ptr<raft::device_resources>>{};
 
-    auto streams_per_device = get_gpu_resources().get_streams_per_device();
-
-    auto iter = raft_resources.find(device_id);
-    if (iter == raft_resources.end()) {
+    auto iter = all_resources.find(device_id);
+    if (iter == all_resources.end()) {
         auto scoped_device = device_setter{device_id};
-        raft_resources[device_id] = raft::device_resources {
-            detail::get_gpu_resources().get_stream_view(device_id, get_thread_id()), {nullptr},
-                rmm::mr::get_current_device_resource()
-        }
+        all_resources[device_id] = std::make_unique<raft::device_resources>(
+            get_gpu_resources().get_stream_view(), nullptr, rmm::mr::get_current_device_resource());
     }
-    return raft_resources[device_id];
+    return *all_resources[device_id];
 }
 
 };  // namespace raft_utils
