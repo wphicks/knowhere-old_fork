947e4af2e175f892639e1983c6a4bfbf4aed399c
William Hicks <whicks@nvidia.com>
2023-11-01 11:41:33 -0400

    Use branch with bugfix for IVF Flat filtering

--- cmake/libs/libraft.cmake
+++ cmake/libs/libraft.cmake
@@ -15,8 +15,8 @@
 
 add_definitions(-DKNOWHERE_WITH_RAFT)
 set(RAFT_VERSION "${RAPIDS_VERSION}")
-set(RAFT_FORK "rapidsai")
-set(RAFT_PINNED_TAG "branch-${RAPIDS_VERSION}")
+set(RAFT_FORK "wphicks")
+set(RAFT_PINNED_TAG "bug-ivf_flat_filter")
 
 
 rapids_find_package(CUDAToolkit REQUIRED
